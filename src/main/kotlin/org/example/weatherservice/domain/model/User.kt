bc06959aecb2b11154a2d01b259ad15a89e38f9e
gdblack <gdblackx@me.com>
2025-12-17 22:57:33 -0500

    build out user, auth dto and jwt util for authentication

--- src/main/kotlin/org/example/weatherservice/domain/model/User.kt
+++ src/main/kotlin/org/example/weatherservice/domain/model/User.kt
@@ -1,6 +1,9 @@
 package org.example.weatherservice.domain.model
 
 import jakarta.persistence.*
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
 import java.time.LocalDateTime
 
 @Entity
@@ -19,8 +22,36 @@ class User (
     @Column(nullable = false)
     val password: String,
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = [JoinColumn(name = "user_id")])
+    @Column(name = "role")
+    val roles: Set<String> = setOf("ROLE_USER"),
+
     val createdAt: LocalDateTime = LocalDateTime.now(),
 
-    ) {
+    @Column(nullable = false)
+    private val enabled: Boolean = true,
+
+    @Column(nullable = false)
+    private val accountNonExpired: Boolean = true,
+
+    @Column(nullable = false)
+    private val accountNonLocked: Boolean = true,
+
+    @Column(nullable = false)
+    private val credentialsNonExpired: Boolean = true,
+    // UserDetails is a Spring Security interface
+    ): UserDetails {
+
+    override fun getAuthorities(): Collection<GrantedAuthority> {
+        return roles.map { SimpleGrantedAuthority(it) }
+    }
+
+    override fun getPassword(): String = password
+    override fun getUsername(): String = username
+    override fun isAccountNonExpired(): Boolean = accountNonExpired
+    override fun isAccountNonLocked(): Boolean = accountNonLocked
+    override fun isCredentialsNonExpired(): Boolean = credentialsNonExpired
+    override fun isEnabled(): Boolean = enabled
 
 }
